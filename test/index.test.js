a2756a5cc6c123e920ecf6ce62fc1b01f33c067f
csh <csh@duksung.ac.kr>
2025-12-04 09:44:12 +0900

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+const factorial = require('../src/index');
+
+describe('factorial function', () => {
+    it('correctly computes factorial of a positive integer number', () => {
+        expect(factorial(3)).toEqual(6);
+        expect(factorial(5)).toEqual(120);
+    });
+
+
+    it('throws an error if a negative input is provided', () => {
+        expect(() => { 
+            factorial(-1);
+        }).toThrow('n must be a positive integer');
+    });
+});
