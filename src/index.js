dba4351a3899389bd5aa26dedae1016cf02d0309
csh <csh@duksung.ac.kr>
2025-12-04 10:00:56 +0900

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // computes a factorial of a given positive integer
 function factorial(n) {
 
+    /*
     if( n < 0 ) {
         throw new Error('n must be a positive integer');
     }
+        */
 
 
     if( n === 0 ) {
